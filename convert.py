435312af6c2d62752aea319e5621a7552592a54b
bombasticSlacks <dillon.matchett@gmail.com>
2024-03-05 10:13:02 -0400

    Update Scripts

--- convert.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#makes anchor links all lowercase
-import fileinput
-import re
-
-for line in fileinput.input(encoding="utf-8", inplace=True, backup='.bak'):
-    print(re.sub(r'(\(.*?)(\#.*?\))',
-                 lambda m: m.group(1) + m.group(2).lower(), line), end='')
