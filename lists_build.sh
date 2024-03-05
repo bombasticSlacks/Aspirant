21029e61bc5f2407be3754bdca9002460d938001
bombasticSlacks <dillon.matchett@gmail.com>
2024-03-05 11:27:55 -0400

    Add the missing list entry

--- lists_build.sh
+++ lists_build.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 # builds all lists
 python3 ./Game/Core/Scripts/lists.py Game/Core/Weapon-Traits.md Weapon-Traits Game/Core/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Core/Weapon-Traits.md Weapon-Traits Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Core/Armour-Traits.md Armour-Traits Game/Core/Blocks
-
-
-
-
-
-
-
-
-
+python3 ./Game/Core/Scripts/lists.py Game/Weapon-Templates.md Weapon-Templates Game/Core/Blocks
