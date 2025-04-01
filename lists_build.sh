f364086fd418c72c5ef5c552b24e33a5585b83bf
Dillon Matchett <dillon.matchett@gmail.com>
2025-04-01 19:44:47 -0300

    Update lists_build.sh

--- lists_build.sh
+++ lists_build.sh
@@ -13,6 +13,7 @@ python3 ./Game/Core/Scripts/lists.py Game/Core/Attributes.md Secondary%20Attribu
 
 # Aspirant Lists
 python3 ./Game/Core/Scripts/lists.py Game/Trainings-List.md General Game/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Trainings-List.md General,Specialized Game/Worlds/Oddities
 python3 ./Game/Core/Scripts/lists.py Game/Equipment-List.md Weapon,Armour Game/Gear
 python3 ./Game/Core/Scripts/lists.py Game/Weapon-Templates.md Weapon-Templates Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Armour-Templates.md Armour-Templates Game/Blocks
