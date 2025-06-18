afe6fe04b9f08d2b491ef0ff38fb4602a5ec3f73
Dillon Matchett <me@dillonmatchett.com>
2025-06-18 17:41:46 -0600

    more list cleanup

--- lists_build.sh
+++ lists_build.sh
@@ -12,8 +12,8 @@ python3 ./Game/Core/Scripts/lists.py Game/Core/Attributes.md Secondary%20Attribu
 
 
 # Aspirant Lists
-python3 ./Game/Core/Scripts/lists.py Game/Trainings-List.md General,Discipline,Concept Game/Blocks
-python3 ./Game/Core/Scripts/lists.py Game/Trainings-List.md General,Specialized Game/Worlds/Oddities
+python3 ./Game/Core/Scripts/lists.py Game/Trainings-List.md Discipline,Concept Game/Blocks
+python3 ./Game/Core/Scripts/lists.py Game/Trainings-List.md Specialized Game/Worlds/Oddities
 python3 ./Game/Core/Scripts/lists.py Game/Equipment-List.md Weapon,Armour,Gear Game/Gear
 python3 ./Game/Core/Scripts/lists.py Game/Weapon-Templates.md Weapon-Templates Game/Blocks
 python3 ./Game/Core/Scripts/lists.py Game/Armour-Templates.md Armour-Templates Game/Blocks
